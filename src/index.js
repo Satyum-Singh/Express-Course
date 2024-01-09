117b64f3317c9485e02391d8d2b5d82487fea7f4
Satyum <satyumspathania@gmail.com>
2024-01-09 15:44:49 +0530

    learned about middleware

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+// Middleware - it is function which is executed between the request and the response
+//            - it is used to modify the request or the response
+//            - it is used to execute code before the endpoint is executed
+//            - it has 3 parameters (req, res, next) = next function is used to execute the next middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Creating a global middleware which is executed for every request
+app.use((req, res, next) => {
+  console.log(`${req.method} :: ${req.path} :: ${req.ip}`);
+  next();
+});
+
+// Creating Server
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 const groceries = [
@@ -22,10 +33,20 @@ const groceries = [
   },
 ];
 
-app.get("/groceries", (req, res) => {
-  res.send(groceries);
-});
+// GET Request
+app.get(
+  "/groceries",
+  //   Applying middleware to the endpoint
+  (req, res, next) => {
+    console.log("Before handling request");
+    next();
+  },
+  (req, res, next) => {
+    res.send(groceries);
+  }
+);
 
+// POST Request
 app.post("/groceries", (req, res) => {
   console.log(req.body);
   groceries.push(req.body);
