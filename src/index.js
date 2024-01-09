9ef84b483676dd1ae2a9be35ab7cc9e8e4ae2424
Satyum <satyumspathania@gmail.com>
2024-01-09 14:07:42 +0530

    Learned Get request.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3001;
 
-app.listen(port,()=>console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get("/groceries", (req, res) => {
+  res.send([
+    {
+      item: "Hot CHocolate",
+      quantity: 3,
+    },
+    {
+      item: "Kaffee",
+      quantity: 1,
+    },
+    {
+      item: "Milch",
+      quantity: 2,
+    },
+  ]);
+});
